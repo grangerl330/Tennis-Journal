47c45c7416fdc13bd8c36ad21187463a4786ca9d
grangerl330 <grangerl330@gmail.com>
2019-06-13 15:00:14 -0400

    moved Matches.js and MatchForm.js to containers folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Matches from './components/Matches'
-import MatchForm from './components/MatchForm'
+import Matches from './containers/Matches'
+import MatchForm from './containers/MatchForm'
 import Navbar from './components/Navbar'
 import './App.css';
 
--- client/src/containers/Matches.js
+++ client/src/containers/Matches.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { fetchMatches } from '../actions/matches'
 import { connect } from 'react-redux'
 
