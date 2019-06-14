d227d68878d8625badf0c4751629d302426b9e60
grangerl330 <grangerl330@gmail.com>
2019-06-14 18:55:42 -0400

    added welcome message to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Logout from '../containers/Logout'
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <div className="navbar">
+      {`Welcome ${props.currentUser.first_name}`}
       <Logout />
     </div>
   )
 }
 
-export default Navbar
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
