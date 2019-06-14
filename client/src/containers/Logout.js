c9a1ee91d0fdc02467255a611958bb3bebcf253d
grangerl330 <grangerl330@gmail.com>
2019-06-14 18:22:30 -0400

    clean up Logout.js

--- client/src/containers/Logout.js
+++ client/src/containers/Logout.js
@@ -3,13 +3,11 @@ import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser'
 
 const Logout = ({ logout }) => {
-
   const handleOnSubmit = event => {
     event.preventDefault()
 
     logout()
   }
-
 
   return (
     <form onSubmit={handleOnSubmit}>
