42d7a8903db10721727df900d494428a0ca2d17d
grangerl330 <grangerl330@gmail.com>
2019-06-14 14:22:44 -0400

    added create method in sessions controller and current user + logged in in application controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::SessionsController < ApplicationController
 
   def create
-    
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:email])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/api/login', to: 'sessions#create'
+  post '/api/login', to: 'api/sessions#create'
 
   namespace :api do
     resources :matches
