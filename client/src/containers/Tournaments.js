58db77f56d6236f721977ee4325e0d7f32dc45d2
grangerl330 <grangerl330@gmail.com>
2019-06-14 22:10:33 -0400

    added Tournaments component

---
+++ client/src/containers/Tournaments.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { fetchTournaments } from '../actions/tournaments'
+import { connect } from 'react-redux'
+
+class Tournaments extends Component {
+
+  componentDidMount() {
+    this.props.fetchTournaments()
+  }
+
+  render() {
+    const renderTournaments = this.props.tournaments.map(tournament =>
+      <div key={tournament.id}>
+      </div>
+    )
+
+    return (
+      <div>
+        <h2>Tournaments</h2>
+        {renderTournaments}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    tournaments: state.tournaments
+  }
+}
+
+export default connect(mapStateToProps, { fetchTournaments })(Tournaments)
