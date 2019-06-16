c540adf7ad7e08f6c4ce6cd1806cde484ac81695
grangerl330 <grangerl330@gmail.com>
2019-06-16 17:02:49 -0400

    added additional fields to match form

--- client/src/components/MatchForm.js
+++ client/src/components/MatchForm.js
@@ -5,9 +5,12 @@ class MatchForm extends Component {
     super()
 
     this.state = {
+      date: "",
+      time: "",
       round: "",
       result: "",
-      score: ""
+      score: "",
+      notes: ""
     }
   }
 
@@ -27,7 +30,8 @@ class MatchForm extends Component {
     this.setState({
       round: "",
       result: "",
-      score: ""
+      score: "",
+      date: ""
     })
   }
 
@@ -36,6 +40,14 @@ class MatchForm extends Component {
       <div>
         <form onSubmit={this.handleOnSubmit}>
           <h2>Add Match</h2>
+          <p>
+            <label htmlFor="match-date">Date</label>
+            <input type="date" name="date" value={this.state.date} onChange={this.handleOnChange} />
+          </p>
+          <p>
+            <label htmlFor="match-time">Time</label>
+            <input type="time" name="time" value={this.state.time} onChange={this.handleOnChange} />
+          </p>
           <p>
             <label htmlFor="match-round">Round</label>
             <input type="text" name="round" value={this.state.round} onChange={this.handleOnChange} placeholder="Round"/>
@@ -47,6 +59,9 @@ class MatchForm extends Component {
           <p>
             <label htmlFor="match-score">Score</label>
             <input type="text" name="score" value={this.state.score} onChange={this.handleOnChange} placeholder="Score"/>
+          </p>
+          <p>
+            <textarea name="notes" value={this.state.notes} onChange={this.handleOnChange} placeholder="Notes">Notes</textarea>
           </p>
           <button>Add Match</button>
         </form>
