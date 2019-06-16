a56a6dd69607917d452b27a9b175e022e6af7fd8
grangerl330 <grangerl330@gmail.com>
2019-06-16 11:58:31 -0400

    added TournamentCard and MatchCard for diplaying info about individual match or tournament

---
+++ client/src/components/MatchCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MatchCard = (props) => {
+  return (
+    <div className="match-card">
+
+    </div>
+  )
+}
+
+export default MatchCard
---
+++ client/src/components/TournamentCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TournamentCard = (props) => {
+  return (
+    <div className="tournament-card">
+    
+    </div>
+  )
+}
+
+export default TournamentCard
