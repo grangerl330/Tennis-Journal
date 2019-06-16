446c7650e7d456a3fba1bad4ab221438fadcd6fe
grangerl330 <grangerl330@gmail.com>
2019-06-16 10:53:18 -0400

    added BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
