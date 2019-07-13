d0262f485e333df6df873b2e82cb257d95a132a6
grangerl330 <grangerl330@gmail.com>
2019-07-12 22:57:04 -0400

    fixed bug in opponents controller

--- app/controllers/api/opponents_controller.rb
+++ app/controllers/api/opponents_controller.rb
@@ -1,8 +1,8 @@
-class OpponentsController < ApplicationController
+class Api::OpponentsController < ApplicationController
 
   def index
     @opponents = current_user.opponents
     render json: @opponents
   end
-  
+
 end
