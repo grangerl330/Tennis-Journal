15fe448cf8e390a875172fc6193a52f683dbf3e0
grangerl330 <grangerl330@gmail.com>
2019-07-12 22:58:12 -0400

    added last name to opponent links in side bar

--- client/src/components/Opponents.js
+++ client/src/components/Opponents.js
@@ -5,7 +5,7 @@ const Opponents = (props) => {
 
   const renderOpponents = props.opponents.map(opponent =>
     <div key={opponent.id}>
-      <p><NavLink className="sidebarlink" to={`/opponents/${opponent.id}`}>{opponent.first_name}</NavLink></p>
+      <p><NavLink className="sidebarlink" to={`/opponents/${opponent.id}`}>{opponent.first_name} {opponent.last_name}</NavLink></p>
     </div>
   )
 
