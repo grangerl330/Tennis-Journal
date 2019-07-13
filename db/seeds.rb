19392a1d6e46eafc5b41b2ede1107703ce0c82a6
grangerl330 <grangerl330@gmail.com>
2019-07-12 22:56:22 -0400

    added additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,13 @@ User.create(first_name: "User", last_name: "3", email: "user3@gmail.com", passwo
 
 #Tournaments
 Tournament.create(title: "Tournament 1", start_date: nil, end_date: nil, location: "Location 1", surface: "Hard", age_category: "16 and Under", draw_size: 128, points: 500, user_id: 1)
+
+#Matches
+Match.create(round: 128, result: "won", score: "6-0, 6-0", date: "", time: "", notes: "", tournament_id: "1", user_id: "1")
+Match.create(round: 64, result: "won", score: "6-2, 6-3", date: "", time: "", notes: "", tournament_id: "1", user_id: "1")
+Match.create(round: 32, result: "won", score: "6-4, 6-1", date: "", time: "", notes: "", tournament_id: "1", user_id: "1")
+
+#Opponents
+Opponent.create(first_name: "Opponent", last_name: "1", age: 18, handedness: "right", notes: "", utr: 9, match_id: 1)
+Opponent.create(first_name: "Opponent", last_name: "2", age: 15, handedness: "left", notes: "", utr: 10, match_id: 2)
+Opponent.create(first_name: "Opponent", last_name: "3", age: 15, handedness: "right", notes: "", utr: 11, match_id: 3)
