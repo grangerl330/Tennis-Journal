c6a957c726333d5e0570f331dcb36833159e390a
grangerl330 <grangerl330@gmail.com>
2019-07-15 11:24:29 -0400

    updated layout of top bar and made it display the current page the user is on

--- client/src/containers/Topbar.js
+++ client/src/containers/Topbar.js
@@ -2,17 +2,25 @@ import React from 'react'
 import Logout from '../components/Logout'
 import { logout } from '../actions/currentUser'
 import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Switch, Route } from 'react-router-dom'
+import Tournaments from '../components/Tournaments'
+import { fetchTournaments } from '../actions/tournaments'
 
 const Topbar = (props) => {
   return (
     <div className="topbar">
-      <span className="welcome-message">{`Hello ${props.currentUser.first_name}!`} Welcome to Tennis Journal!</span>
-      <Logout logout={props.logout}/>
-      <NavLink to='/profile' className="navlink">Profile</NavLink>
-      <NavLink to="/tournaments/add_tournament" className="navlink">Tournaments</NavLink>
-      <NavLink to="/matches/add_match" className="navlink">Matches</NavLink>
-      <NavLink to="/opponents" className="navlink">Opponents</NavLink>
+      <div className="topbar-logo">
+        Tennis Journal
+      </div>
+      <div className="username-display">
+        {props.currentUser.first_name} {props.currentUser.last_name}
+        <div className="logout-display">
+          <Logout logout={props.logout}/>
+        </div>
+      </div>
+      <Route path='/tournaments' render={() => <div className="page-name-display">Tournaments</div>}/>
+      <Route path='/matches' render={() => <div className="page-name-display">Matches</div>}/>
+      <Route path='/opponents' render={() => <div className="page-name-display">Opponents</div>}/>
     </div>
   )
 }
