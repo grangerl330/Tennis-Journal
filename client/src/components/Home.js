18fed5b4eb5a80554cf9e39168a7aaac17a828c6
grangerl330 <grangerl330@gmail.com>
2019-07-16 19:59:25 -0400

    added new data to user home page display

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,11 +4,10 @@ const Home = (props) => {
   return (
     <div className="main-content-text">
       <h2>Home Page</h2>
-      <p>First Name: {props.currentUser.first_name}</p>
-      <p>Last Name: {props.currentUser.last_name}</p>
-      <p>Record: <i>user's record</i></p>
-      <p>Current Ranking: <i>user's record</i></p>
-      <p>UTR: <i>user's UTR</i></p>
+      <p><b>{props.currentUser.first_name} {props.currentUser.last_name}</b></p>
+      <p>Record: {props.currentUser.record}</p>
+      <p>Current Ranking: {props.currentUser.ranking}</p>
+      <p>UTR: {props.currentUser.utr}</p>
     </div>
   )
 }
