dc8919717c3829d10f07c191851e845a77c1ab8d
grangerl330 <grangerl330@gmail.com>
2019-07-15 21:54:49 -0400

    added navlinks to sidebar

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -1,40 +1,14 @@
-import React, { Component } from 'react';
-import Matches from '../components/Matches'
-import { fetchMatches } from '../actions/matches'
-import Tournaments from '../components/Tournaments'
-import { fetchTournaments } from '../actions/tournaments'
-import Opponents from '../components/Opponents'
-import { fetchOpponents } from '../actions/opponents'
-import { Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom'
 
-class Sidebar extends Component {
-
-  componentDidMount() {
-    this.props.fetchMatches()
-    this.props.fetchTournaments()
-    this.props.fetchOpponents()
-  }
-
-  render() {
-    return (
-      <div className="sidebar">
-        <Switch>
-          <Route path='/matches' render={() => <Matches matches={this.props.matches}/>}/>
-          <Route path='/tournaments' render={() => <Tournaments tournaments={this.props.tournaments}/>}/>
-          <Route exact path='/opponents' render={() => <Opponents opponents={this.props.opponents}/>}/>
-        </Switch>
-      </div>
-    )
-  }
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <p><NavLink to="/tournaments" className="navlink">Tournaments</NavLink></p>
+      <p><NavLink to="/matches" className="navlink">Matches</NavLink></p>
+      <p><NavLink to="/opponents" className="navlink">Opponents</NavLink></p>
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    matches: state.matches,
-    tournaments: state.tournaments,
-    opponents: state.opponents
-  }
-}
-
-export default connect(mapStateToProps, { fetchMatches, fetchTournaments, fetchOpponents })(Sidebar)
+export default Sidebar
