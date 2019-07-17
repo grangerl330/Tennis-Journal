d61103e108bafe496995289ccb19f035d81ee78c
grangerl330 <grangerl330@gmail.com>
2019-07-16 21:38:42 -0400

    added moment.js

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ApplicationRecord
   belongs_to :user
   has_many :matches
-
+  
 end
--- client/src/components/TournamentCard.js
+++ client/src/components/TournamentCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router';
 
 const TournamentCard = (props) => {
   var tournamentId = parseInt(props.id)
