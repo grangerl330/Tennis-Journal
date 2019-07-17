264221bf5959c8b399feb8bfffc4a447a80d3a2f
grangerl330 <grangerl330@gmail.com>
2019-07-16 23:38:00 -0400

    changed sidebar links to 'All Matches' and 'All Opponents' to be more specific

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -6,8 +6,8 @@ const Sidebar = () => {
     <div className="sidebar">
       <p><NavLink to="/home" className="navlink">Home</NavLink></p>
       <p><NavLink to="/tournaments" className="navlink">Tournaments</NavLink></p>
-      <p><NavLink to="/matches" className="navlink">Matches</NavLink></p>
-      <p><NavLink to="/opponents" className="navlink">Opponents</NavLink></p>
+      <p><NavLink to="/matches" className="navlink">All Matches</NavLink></p>
+      <p><NavLink to="/opponents" className="navlink">All Opponents</NavLink></p>
     </div>
   )
 }
