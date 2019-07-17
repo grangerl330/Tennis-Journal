537e99d3f513952cdb45f8e056b0b84f9175bcdb
grangerl330 <grangerl330@gmail.com>
2019-07-16 20:26:30 -0400

    added home link to sidebar

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      <p><NavLink to="/home" className="navlink">Home</NavLink></p>
       <p><NavLink to="/tournaments" className="navlink">Tournaments</NavLink></p>
       <p><NavLink to="/matches" className="navlink">Matches</NavLink></p>
       <p><NavLink to="/opponents" className="navlink">Opponents</NavLink></p>
