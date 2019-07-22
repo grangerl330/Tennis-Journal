928b2ec2a90139720d3b0dd6d2a88d42ebc7d36c
grangerl330 <grangerl330@gmail.com>
2019-07-22 19:38:47 -0400

    added update method to opponents controller

--- app/controllers/api/opponents_controller.rb
+++ app/controllers/api/opponents_controller.rb
@@ -5,4 +5,21 @@ class Api::OpponentsController < ApplicationController
     render json: @opponents, include: "match,match.tournament"
   end
 
+  def update
+    @opponent = Opponent.find_by_id(params[:opponent][:id])
+
+    if @opponent.update(opponent_params)
+      render json: @opponent
+    else
+      render json: {
+        error: "This Opponent Failed To Update"
+      }
+    end
+  end
+
+  private
+
+  def opponent_params
+    params.require(:opponent).permit(:first_name, :last_name, :age, :handedness, :utr, :notes)
+  end
 end
