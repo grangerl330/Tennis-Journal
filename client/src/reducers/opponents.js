8a33c16f509a2ae1dc1221182bd21df44899fb24
grangerl330 <grangerl330@gmail.com>
2019-07-22 20:37:03 -0400

    set up reducer to add a new match's opponent to the redux store

--- client/src/reducers/opponents.js
+++ client/src/reducers/opponents.js
@@ -2,6 +2,10 @@ export default (state = [], action) => {
   switch(action.type) {
     case 'SET_OPPONENTS':
       return action.opponents
+    case 'ADD_MATCH':
+      var opponent = action.match.opponent
+      opponent.tournament = action.match.tournament
+      return [...state, opponent]
     case 'UPDATE_OPPONENT':
       return [...state.filter(opponent => opponent.id !== action.opponent.id), action.opponent]
     default:
