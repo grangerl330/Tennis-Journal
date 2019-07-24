ee3d448324d568b21d4778d9e8760da7014bea0e
grangerl330 <grangerl330@gmail.com>
2019-07-24 00:09:21 -0400

    made login and signup forms clear when the the current user logs out

--- client/src/reducers/signupForm.js
+++ client/src/reducers/signupForm.js
@@ -9,6 +9,8 @@ export default (state = initialState, action) => {
   switch(action.type) {
     case 'UPDATE_SIGNUP_FORM':
       return action.formData
+    case 'CLEAR_CURRENT_USER':
+      return initialState
     default:
       return state
   }
