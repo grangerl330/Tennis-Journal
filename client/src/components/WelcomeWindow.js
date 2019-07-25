f4baa4a99d6726df0a89c4979bc2f878d25b866b
grangerl330 <grangerl330@gmail.com>
2019-07-25 19:26:03 -0400

    fixed typos in welcome window

--- client/src/components/WelcomeWindow.js
+++ client/src/components/WelcomeWindow.js
@@ -12,22 +12,22 @@ const WelcomeWindow = () => {
         <h2>Welcome To Tennis Journal!</h2>
         <div className="welcome-window-text">
           <p>
-            This app designed to help you keep track of tournaments, matches, and stats.
+            This app is designed to help you keep track of tournaments, matches, and stats.
           </p>
           <p>
-            Add a new tournament by clicking the tournaments link in the side bar.
+            Add a new tournament by clicking the Tournaments link in the side bar.
           </p>
           <p>
             Add a new match to that tournament using the Add Match link.
           </p>
           <p>
-            The All Matches and All Opponents lists will automatically populate when new matches are created
+            The All Matches and All Opponents lists will automatically populate when new matches are created.
           </p>
           <p>
-            Click the stats link to see a summary of all your stats. Record and Points will update when tournaments and matches are updated or created
+            Click the stats link to see a summary of all your stats. Record and Points will update when tournaments and matches are updated or created.
           </p>
           <p>
-            To view your profile, click your name in the top left corner
+            To view your profile, click your name in the top right corner.
           </p>
         </div>
       </div>
