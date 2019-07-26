57f1d70b00958c2d30b12e41d59ab0678051438c
grangerl330 <grangerl330@gmail.com>
2019-07-25 21:53:10 -0400

    added logic to display instructions if user has not entered a ranking or utr on their stats page

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -7,6 +7,16 @@ class Stats extends Component {
     this.props.getCurrentUser()
   }
 
+  instructions = () => {
+    if(!this.props.currentUser.ranking && !this.props.currentUser.utr){
+      return <span className="instructions-display">* Enter your Current Ranking and UTR by clicking Edit</span>
+    } else if(!this.props.currentUser.ranking){
+      return <span className="instructions-display">* Enter your Current Ranking by clicking Edit</span>
+    } else if(!this.props.currentUser.utr){
+      return <span className="instructions-display">* Enter your UTR by clicking Edit</span>
+    }
+  }
+
   render() {
     return (
       <div className="main-content-text">
@@ -14,6 +24,7 @@ class Stats extends Component {
         <p>Current Ranking: {this.props.currentUser.ranking}</p>
         <p>UTR: {this.props.currentUser.utr}</p>
         <p>Points: {this.props.currentUser.points}</p>
+        <p>{this.instructions()}</p>
         <NavLink to={`/stats/edit`} className="button">Edit</NavLink>
         <Route path='/stats/edit' render={() => <StatsForm currentUser={this.props.currentUser} updateCurrentUserInDatabase={this.props.updateCurrentUserInDatabase}/>} />
       </div>
