1c45edc0802e49810fac9085d5392c2ae04d174f
grangerl330 <grangerl330@gmail.com>
2019-11-06 20:52:41 -0500

    added node-sass and custom scss file

--- client/src/containers/LoginPage.js
+++ client/src/containers/LoginPage.js
@@ -10,9 +10,13 @@ import { connect } from 'react-redux'
 const LoginPage = (props) => {
   return (
     <div className="section" id="loginpage">
-      <div className="container">
+      <div className="container my-5">
         <Login loginFormData={props.loginFormData} updateLoginForm={props.updateLoginForm} login={props.login} welcome={props.welcome}/>
-        <h2>or</h2>
+        <div className="row my-3" id="login">
+          <div className="col-md-6 mx-auto text-center">
+            <h2>or</h2>
+          </div>
+        </div>
         <Signup signupFormData={props.signupFormData} updateSignupForm={props.updateSignupForm} signup={props.signup} welcome={props.welcome}/>
       </div>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
 import './stylesheets/index.css';
 import App from './App';
 import { Provider } from 'react-redux'
