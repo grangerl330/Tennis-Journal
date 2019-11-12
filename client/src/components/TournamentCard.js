fdbc76049f1f89d6c9a3dfe3c26a7a7c4f538cc7
grangerl330 <grangerl330@gmail.com>
2019-11-12 00:35:33 -0500

    updates to tournament card page

--- client/src/components/TournamentCard.js
+++ client/src/components/TournamentCard.js
@@ -54,134 +54,124 @@ const TournamentCard = (props) => {
         </div>
 
         <div className="container-fluid px-4">
-          <div className="row justify-content-center mt-2">
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
-                <div className="card-body">
-                  <div className="row justify-content-center">
-                    <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-left">
-                      <i className="fas fa-calendar-alt fa-3x home-icon"></i>
-                    </div>
-                    <div className="col-6 mx-0 px-0">
-                      <div className="row justify-content-center mx-0">
-                        <h5 className="card-title">{moment(tournament.start_date).format('MM/DD/YY')} - {moment(tournament.end_date).format('MM/DD/YY')}</h5>
-                      </div>
-                      <div className="row justify-content-center mx-0">
-                        <p className="card-text font-italic">Dates</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
-                  <div className="card-body">
-                    <div className="row justify-content-center">
-                      <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-center">
-                        <i className="fas fa-map-marker-alt fa-3x home-icon"></i>
-                      </div>
-                      <div className="col-6 mx-0 pl-4 px-lg-0">
-                        <div className="row justify-content-center mx-0">
-                          <h5 className="card-title">{tournament.location}</h5>
-                        </div>
-                        <div className="row justify-content-center mx-0">
-                          <p className="card-text font-italic">Location</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
-                <div className="card-body">
-                  <div className="row justify-content-center">
-                    <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-center">
-                      <img src={ageIcon} alt="age-icon"/>
-                    </div>
-                    <div className="col-6 mx-0 px-0">
-                      <div className="row justify-content-center mx-0">
-                        <h5 className="card-title">{tournament.age_category}</h5>
-                      </div>
-                      <div className="row justify-content-center mx-0">
-                        <p className="card-text font-italic">Ages</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
+          <label className="btn w-100" data-toggle="modal" data-target="#tournamentFormModal">
+            <div className="row justify-content-center mt-2">
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
                   <div className="card-body">
                     <div className="row justify-content-center">
                       <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-left">
-                        <img src={tennisCourtIcon} alt="tennis-court-icon"/>
+                        <i className="fas fa-calendar-alt fa-3x home-icon"></i>
                       </div>
-                      <div className="col-6 mx-0 pl-4 px-lg-0">
+                      <div className="col-6 mx-0 px-0">
                         <div className="row justify-content-center mx-0">
-                          <h5 className="card-title">{tournament.surface}</h5>
+                          <h5 className="card-title">{moment(tournament.start_date).format('MM/DD/YY')} - {moment(tournament.end_date).format('MM/DD/YY')}</h5>
                         </div>
                         <div className="row justify-content-center mx-0">
-                          <p className="card-text font-italic">Surface</p>
+                          <p className="card-text font-italic">Dates</p>
                         </div>
                       </div>
                     </div>
                   </div>
+                </div>
               </div>
-            </div>
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
-                <div className="card-body">
-                  <div className="row justify-content-center">
-                  <div className="col-3 col-md-6 p-0 text-right">
-                    <img src={tournamentDrawIcon} alt="tournament-draw-icon"/>
-                  </div>
-                    <div className="col-6 mx-0 px-0">
-                      <div className="row justify-content-center mx-0">
-                        <h5 className="card-title">{tournament.draw_size}</h5>
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
+                    <div className="card-body">
+                      <div className="row justify-content-center">
+                        <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-center">
+                          <i className="fas fa-map-marker-alt fa-3x home-icon"></i>
+                        </div>
+                        <div className="col-6 mx-0 pl-4 px-lg-0">
+                          <div className="row justify-content-center mx-0">
+                            <h5 className="card-title">{tournament.location}</h5>
+                          </div>
+                          <div className="row justify-content-center mx-0">
+                            <p className="card-text font-italic">Location</p>
+                          </div>
+                        </div>
                       </div>
-                      <div className="row justify-content-center mx-0">
-                        <p className="card-text font-italic">Draw Size</p>
+                    </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
+                  <div className="card-body">
+                    <div className="row justify-content-center">
+                      <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-center">
+                        <img src={ageIcon} alt="age-icon"/>
+                      </div>
+                      <div className="col-6 mx-0 px-0">
+                        <div className="row justify-content-center mx-0">
+                          <h5 className="card-title">{tournament.age_category}</h5>
+                        </div>
+                        <div className="row justify-content-center mx-0">
+                          <p className="card-text font-italic">Ages</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
+                    <div className="card-body">
+                      <div className="row justify-content-center">
+                        <div className="col-3 col-md-6 px-0 pb-2 pt-1 text-center">
+                          <img src={tennisCourtIcon} alt="tennis-court-icon"/>
+                        </div>
+                        <div className="col-6 mx-0 pl-4 px-lg-0">
+                          <div className="row justify-content-center mx-0">
+                            <h5 className="card-title">{tournament.surface}</h5>
+                          </div>
+                          <div className="row justify-content-center mx-0">
+                            <p className="card-text font-italic">Surface</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
+                  <div className="card-body">
+                    <div className="row justify-content-center">
+                    <div className="col-3 col-md-6 pt-2 px-0 text-right">
+                      <img src={tournamentDrawIcon} alt="tournament-draw-icon"/>
+                    </div>
+                      <div className="col-6 mx-0 px-0">
+                        <div className="row justify-content-center mx-0">
+                          <h5 className="card-title">{tournament.draw_size}</h5>
+                        </div>
+                        <div className="row justify-content-center mx-0">
+                          <p className="card-text font-italic">Draw Size</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2">
+                <div className="card card-stats text-center border-0">
+                  <div className="card-body">
+                    <div className="row justify-content-center">
+                      <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-right">
+                        <i className="far fa-dot-circle fa-3x home-icon"></i>
+                      </div>
+                      <div className="col-6 mx-0 px-0">
+                        <div className="row justify-content-center mx-0">
+                          <h5 className="card-title">{tournament.points}</h5>
+                        </div>
+                        <div className="row justify-content-center mx-0">
+                          <p className="card-text font-italic">Points</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-md-2">
-              <div className="card card-stats text-center border-0">
-                <div className="card-body">
-                  <div className="row justify-content-center">
-                    <div className="col-3 col-md-6 px-0 pb-0 pt-2 text-right">
-                      <i className="far fa-dot-circle fa-3x home-icon"></i>
-                    </div>
-                    <div className="col-6 mx-0 px-0">
-                      <div className="row justify-content-center mx-0">
-                        <h5 className="card-title">{tournament.points}</h5>
-                      </div>
-                      <div className="row justify-content-center mx-0">
-                        <p className="card-text font-italic">Points</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col-md-2">
-              <button className="btn" data-toggle="modal" data-target="#tournamentFormModal">
-                <i className="fas fa-edit"></i><span className="ml-2 font-italic">Edit Tournament</span>
-              </button>
-            </div>
-            <div className="col-md-3">
-              <button className="btn btn-danger btn-block" data-toggle="modal" data-target="#deleteTournamentModal">
-                <i className="fas fa-trash"></i> Delete Tournament
-              </button>
-            </div>
-          </div>
+          </label>
         </div>
 
         <div className="container-fluid my-5">
@@ -199,9 +189,23 @@ const TournamentCard = (props) => {
           </div>
         </div>
 
-        <div className="container-fluid mb-5">
+        <div className="container-fluid mb-3">
           <div className="card-deck mt-4">
             {renderMatches}
+          </div>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row my-5 justify-content-center">
+            <div className="col-md-3">
+              <button className="btn btn-danger btn-block" data-toggle="modal" data-target="#deleteTournamentModal">
+                <i className="fas fa-trash"></i> Delete Tournament
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid">
+          <div className="row my-5">
           </div>
         </div>
           <MatchFormModal tournamentId={tournament.id} sendMatchToDatabase={props.addMatchToDatabase} matches={props.matches} tournament={tournament} add="add"/>
