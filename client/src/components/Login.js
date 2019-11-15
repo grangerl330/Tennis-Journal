d46005538a02ff2e9766d9400d5649a29257266e
grangerl330 <grangerl330@gmail.com>
2019-11-15 13:51:42 -0500

    added form validations to login and signup forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,50 +1,99 @@
-import React from 'react'
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import validator from 'validator'
 import { withRouter } from 'react-router';
 
-const Login = (props) => {
+class Login extends Component {
 
-  const handleOnChange = event => {
-    const { name, value } = event.target
-    const updatedFormInfo = {
-      ...props.loginFormData,
-      [name]: value
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email: "",
+      emailIsValid: true,
+      password: "",
+      passwordIsValid: true
     }
-    props.updateLoginForm(updatedFormInfo)
   }
 
-  const handleOnSubmit = event => {
+  handleOnChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleOnSubmit = event => {
     event.preventDefault()
 
-    props.login(props.loginFormData)
+    this.resetValidations()
+
+    if (this.formIsValid()) {
+      this.props.login(this.state)
 
 
-    props.history.push('/home')
+      this.props.history.push('/home')
+    }
   }
 
-  return (
-    <div className="row" id="login">
-      <div className="col-md-6 mx-auto">
-        <div className="card">
-          <div className="card-header">
-            <h4>Login</h4>
-          </div>
-          <div className="card-body">
-            <form onSubmit={handleOnSubmit}>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input className="form-control" type="text" name="email" onChange={handleOnChange} value={props.loginFormData.email} placeholder="email" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input className="form-control" type="password" name="password" onChange={handleOnChange} value={props.loginFormData.password} placeholder="password" />
-              </div>
-              <input className="btn btn-dark btn-block" type="submit" value="Login" />
-            </form>
+  formIsValid = () => {
+    let isValid = true;
+
+    if (!validator.isEmail(this.state.email)) {
+    this.setState({
+      emailIsValid: false
+    })
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  resetValidations = () => {
+    this.setState({
+      emailIsValid: true,
+      passwordIsValid: true,
+      passwordConfirmationIsValid: true
+    })
+  }
+
+  render() {
+    const emailInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.emailIsValid }
+    );
+
+    const passwordInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.passwordIsValid }
+    );
+
+    return (
+      <div className="row" id="login">
+        <div className="col-md-6 mx-auto">
+          <div className="card">
+            <div className="card-header">
+              <h4>Login</h4>
+            </div>
+            <div className="card-body">
+              <form onSubmit={this.handleOnSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input className={emailInputClass} type="text" name="email" onChange={this.handleOnChange} value={this.state.email} placeholder="Email" />
+                  <div className="invalid-feedback">
+                    Not a valid email address
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input className={passwordInputClass} type="password" name="password" onChange={this.handleOnChange} value={this.state.password} placeholder="Password" />
+                </div>
+                <input className="btn btn-dark btn-block" type="submit" value="Login" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default withRouter(Login)
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,56 +1,127 @@
-import React from 'react'
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import validator from 'validator'
 import { withRouter } from 'react-router';
 
-const Signup = (props) => {
-  const handleOnSubmit = event => {
+class Signup extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      emailIsValid: true,
+      password: "",
+      passwordConfirmation: "",
+      passwordConfirmationIsValid: true
+    }
+  }
+
+  handleOnChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleOnSubmit = event => {
     event.preventDefault()
 
-    props.signup(props.signupFormData)
+    this.resetValidations()
 
-    props.history.push('/home')
-  }
+    if (this.formIsValid()) {
+      this.props.signup(this.state)
 
-  const handleOnChange = event => {
-    const { name, value } = event.target
-    const updatedFormInfo = {
-      ...props.signupFormData,
-      [name]: value
+
+      this.props.history.push('/home')
     }
-    props.updateSignupForm(updatedFormInfo)
   }
 
-  return (
-    <div className="row" id="login">
-      <div className="col-md-6 mx-auto">
-        <div className="card">
-          <div className="card-header">
-            <h4>Sign Up</h4>
-          </div>
-          <div className="card-body">
-            <form onSubmit={handleOnSubmit}>
-              <div className="form-group">
-                <label htmlFor="First Name">First Name</label>
-                <input className="form-control" type="text" name="first_name" onChange={handleOnChange} value={props.signupFormData.firstName} placeholder="First Name" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="Last Name">Last Name</label>
-                <p><input className="form-control" type="text" name="last_name" onChange={handleOnChange} value={props.signupFormData.lastName} placeholder="Last Name" /></p>
-              </div>
-              <div className="form-group">
-                <label htmlFor="Email">Email</label>
-                <p><input className="form-control" type="text" name="email" onChange={handleOnChange} value={props.signupFormData.email} placeholder="Email" /></p>
-              </div>
-              <div className="form-group">
-                <label htmlFor="Password">Password</label>
-                <p><input className="form-control" type="password" name="password" onChange={handleOnChange} value={props.signupFormData.password} placeholder="Password" /></p>
-              </div>
-                <input className="btn btn-dark btn-block" type="submit" value="Sign Up" />
-            </form>
+  formIsValid = () => {
+    let isValid = true;
+
+    if (!validator.isEmail(this.state.email)) {
+    this.setState({
+      emailIsValid: false
+    })
+      isValid = false;
+    }
+
+    if(this.state.password !== this.state.passwordConfirmation) {
+      this.setState({
+        passwordConfirmationIsValid: false
+      })
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  resetValidations = () => {
+    this.setState({
+      emailIsValid: true,
+      passwordConfirmationIsValid: true
+    })
+  }
+
+  render() {
+    const emailInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.emailIsValid }
+    );
+
+    const passwordInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.passwordIsValid }
+    );
+
+    const passwordConfirmationInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.passwordConfirmationIsValid }
+    );
+
+    return (
+      <div className="row" id="login">
+        <div className="col-md-6 mx-auto">
+          <div className="card">
+            <div className="card-header">
+              <h4>Sign Up</h4>
+            </div>
+            <div className="card-body">
+              <form onSubmit={this.handleOnSubmit}>
+                <div className="form-group">
+                  <label htmlFor="First Name">First Name</label>
+                  <input className="form-control" type="text" name="first_name" onChange={this.handleOnChange} value={this.state.firstName} placeholder="First Name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Last Name">Last Name</label>
+                  <p><input className="form-control" type="text" name="last_name" onChange={this.handleOnChange} value={this.state.lastName} placeholder="Last Name" /></p>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input className={emailInputClass} type="text" name="email" onChange={this.handleOnChange} value={this.state.email} placeholder="Email" />
+                  <div className="invalid-feedback">
+                    Not a valid email address
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Password">Password</label>
+                  <p><input className="form-control" type="password" name="password" onChange={this.handleOnChange} value={this.state.password} placeholder="Password" /></p>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password-confirmation">Confirm Password</label>
+                  <input className={passwordConfirmationInputClass} type="password" name="passwordConfirmation" onChange={this.handleOnChange} value={this.state.passwordConfirmation} placeholder="Password Confirmation" />
+                  <div className="invalid-feedback">
+                    Password Does Not Match
+                  </div>
+                </div>
+                  <input className="btn btn-dark btn-block" type="submit" value="Sign Up" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default withRouter(Signup)
