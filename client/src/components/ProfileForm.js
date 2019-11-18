fd7ac3bef78162716a3ad9ba3ee50f7c5a0fbdd8
grangerl330 <grangerl330@gmail.com>
2019-11-18 14:57:43 -0500

    added validations to profile form

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import UpdateSuccessModal from './modals/UpdateSuccessModal'
+import $ from "jquery";
+import classNames from 'classnames'
+import validator from 'validator'
 import { withRouter } from 'react-router';
 
 class ProfileForm extends Component {
@@ -7,8 +11,11 @@ class ProfileForm extends Component {
 
     this.state = {
       first_name: props.currentUser.first_name,
+      firstNameIsValid: true,
       last_name: props.currentUser.last_name,
+      lastNameIsValid: true,
       email: props.currentUser.email,
+      emailIsValid: true,
       id: props.currentUser.id
     }
   }
@@ -23,30 +30,93 @@ class ProfileForm extends Component {
   handleOnSubmit = event => {
     event.preventDefault()
 
-    const user = this.state
-    this.props.updateCurrentUserInDatabase(user)
+    this.resetValidations()
 
-    this.props.history.push('/profile/edit')
+    if(this.formIsValid()) {
+      const user = this.state
+      this.props.updateCurrentUserInDatabase(user)
+
+      $("#updateSuccessModal").modal("show")
+    }
+  }
+
+  formIsValid = () => {
+    let isValid = true;
+
+    if(this.state.first_name === "") {
+      this.setState({
+        firstNameIsValid: false
+      })
+
+      isValid = false;
+    }
+
+    if(this.state.last_name === "") {
+      this.setState({
+        lastNameIsValid: false
+      })
+
+      isValid = false;
+    }
+
+    if (!validator.isEmail(this.state.email)) {
+    this.setState({
+      emailIsValid: false
+    })
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  resetValidations = () => {
+    this.setState({
+      emailIsValid: true,
+      firstNameIsValid: true,
+      lastNameIsValid: true
+    })
   }
 
   render(){
+    const emailInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.emailIsValid }
+    );
+
+    const firstNameInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.firstNameIsValid }
+    );
+
+    const lastNameInputClass = classNames('form-control',
+      { 'is-invalid': !this.state.lastNameIsValid }
+    );
+
     return(
       <div id="profile-form">
         <form onSubmit={this.handleOnSubmit}>
           <div className="form-group">
             <label htmlFor="First Name">First Name:</label>
-            <input className="form-control" type="text" name="first_name" value={this.state.first_name} onChange={this.handleOnChange}/>
+            <input className={firstNameInputClass} type="text" name="first_name" value={this.state.first_name} onChange={this.handleOnChange}/>
+            <div className="invalid-feedback">
+              Please enter your first name
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="Last Name">Last Name:</label>
-            <input className="form-control" type="text" name="last_name" value={this.state.last_name} onChange={this.handleOnChange}/>
+            <input className={lastNameInputClass} type="text" name="last_name" value={this.state.last_name} onChange={this.handleOnChange}/>
+            <div className="invalid-feedback">
+              Please enter your last name
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="Email">Email:</label>
-            <input className="form-control" type="text" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+            <input className={emailInputClass} type="text" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+            <div className="invalid-feedback">
+              Please enter a valid email address
+            </div>
           </div>
           <button className="btn btn-dark mt-2">Update Profile</button>
         </form>
+        <UpdateSuccessModal attribute="Profile" />
       </div>
     )
   }
