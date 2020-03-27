c128ea095576edfc7d3e6d54754016c990392783
grangerl330 <grangerl330@gmail.com>
2020-03-27 12:02:59 -0400

    update rankings chart

--- client/src/components/home-page/RankingsChart.js
+++ client/src/components/home-page/RankingsChart.js
@@ -10,7 +10,7 @@ class RankingsChart extends PureComponent {
         <LineChart data={this.props.data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="month" />
-          <YAxis />
+          <YAxis type="number" domain={[1, 'dataMax + 10']} reversed={true}/>
           <Tooltip />
           <Line type="monotone" dataKey="Rank" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
