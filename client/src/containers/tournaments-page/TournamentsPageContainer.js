5593896de5c085d70ecc1c7808b2e040890b8228
grangerl330 <grangerl330@gmail.com>
2020-04-06 13:30:15 -0400

    add search bar field and add tournament button to tournaments page

--- client/src/containers/tournaments-page/TournamentsPageContainer.js
+++ client/src/containers/tournaments-page/TournamentsPageContainer.js
@@ -30,12 +30,24 @@ const TournamentsPageContainer = (props) => {
               <h2 className="text-green">Tournaments</h2>
             </div>
           </div>
-          <div className="row justify-content-center">
-            <div className="card-deck mt-4 d-flex justify-content-center">
-              {renderTournaments}
+          <div className="row mt-4 pt-3 justify-content-center">
+            <div className="col-3 ml-5 mr-auto">
+              <div className="input-group">
+                <input id="example-search-input" className="search-input form-control py-2 pl-4 border-right-0 border" type="text" placeholder="Search"/>
+                <span className="input-group-append">
+                  <div className="input-group-text search-input-addon bg-transparent"><i className="fa fa-search"></i></div>
+                </span>
+              </div>
+            </div>
+            <div className="col-2 mr-5 ml-auto">
+              <button className="btn btn-green d-block px-4">
+                <NavLink to="/tournaments/add" className="text-white">
+                  Add Tournament
+                </NavLink>
+              </button>
             </div>
           </div>
-          <div className="row justify-content-center mt-4">
+          <div className="row justify-content-center mt-5">
             <div className="col-md-3">
               <NavLink to="/tournaments/add" className="btn btn-dark btn-block">
                 <i className="fas fa-plus"></i> Add Tournament
