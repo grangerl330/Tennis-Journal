5031d4a378f5c98db45a761be4e68e7ff71a7a47
grangerl330 <grangerl330@gmail.com>
2020-04-06 18:42:19 -0400

    add search function for tournaments page

--- client/src/containers/tournaments-page/TournamentsPageContainer.js
+++ client/src/containers/tournaments-page/TournamentsPageContainer.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tournamentIcon from '../../images/tournament-icon.png'
-import moment from 'moment'
 import TournamentsTable from './components/TournamentsTable'
 import { NavLink } from 'react-router-dom'
 
 const TournamentsPageContainer = (props) => {
+
+    const[search, setSearch] = useState("")
+
+    const handleOnChange = event => {
+      const {value} = event.target
+
+      setSearch(value)
+    }
+
+    const filteredTournaments = () => {
+      if(search === "") {
+        return props.tournaments
+      } else {
+        return (
+          props.tournaments.filter(tournament =>
+            Object.keys(tournament).some(key => tournament[key].toString().toLowerCase().includes(search.toLowerCase()))
+          )
+        )
+      }
+    }
+
     return (
       <section id="tournaments-page">
         <div className="container-fluid">
@@ -19,7 +39,7 @@ const TournamentsPageContainer = (props) => {
           <div className="row mt-4 pt-3 justify-content-center">
             <div className="col-3 ml-5 mr-auto">
               <div className="input-group">
-                <input id="example-search-input" className="search-input form-control py-2 pl-4 border-right-0 border" type="text" placeholder="Search"/>
+                <input className="search-input form-control py-2 pl-4 border-right-0 border" type="text" name="search" value={search} onChange={handleOnChange} placeholder="Search"/>
                 <span className="input-group-append">
                   <div className="input-group-text search-input-addon bg-transparent"><i className="fa fa-search"></i></div>
                 </span>
@@ -34,7 +54,7 @@ const TournamentsPageContainer = (props) => {
             </div>
           </div>
           <div className="row mt-5">
-            <TournamentsTable tournaments={props.tournaments}/>
+            <TournamentsTable tournaments={filteredTournaments()} />
           </div>
         </div>
       </section>
--- client/src/containers/tournaments-page/components/TournamentsTable.js
+++ client/src/containers/tournaments-page/components/TournamentsTable.js
@@ -126,33 +126,6 @@ const TournamentsTable = (props) => {
     }
   }];
 
-//   const tournaments = [{
-//     id: 1,
-//     title: "Tournament 1",
-//     dates: "1/1/20 - 1/5/20",
-//     division: "Men's Division",
-//     surface: "Hard",
-//     drawSize: 128,
-//     points: 500
-//   }, {
-//     id: 2,
-//     title: "Tournament 2",
-//     dates: "2/1/20 - 2/5/20",
-//     division: "Men's Division",
-//     surface: "Clay",
-//     drawSize: 32,
-//     points: 100
-//   }, {
-//     id: 3,
-//     title: "Tournament 3",
-//     dates: "3/1/20 - 3/5/20",
-//     division: "Men's Division",
-//     surface: "Grass",
-//     drawSize: 64,
-//     points: 200
-//   }
-// ]
-
   return (
     <div className="col-12 text-center mx-auto">
       <BootstrapTable keyField="id" data={createTournamentsData()} columns={columns} bootstrap4={true} bordered={false} classes="table-borderless" headerClasses="text-green border-0" rowClasses="border-bottom" />
