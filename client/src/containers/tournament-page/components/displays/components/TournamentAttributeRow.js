789758ffcc088a63618d61db87659b1b507b4e83
grangerl330 <grangerl330@gmail.com>
2020-04-26 13:34:59 -0400

    adjust spacing of tournament date row

--- client/src/containers/tournament-page/components/displays/components/TournamentAttributeRow.js
+++ client/src/containers/tournament-page/components/displays/components/TournamentAttributeRow.js
@@ -4,10 +4,10 @@ const TournamentAttributeRow = (props) => {
   if(props.date) {
     return (
       <div className="row mt-3 ml-3 ml-md-5 pr-0 justify-content-start">
-        <div className='col-6 col-md-3 text-green'>
+        <div className='col-6 col-md-4 text-green'>
           <span>{props.name}</span>
         </div>
-        <div className='col-6 col-md-3 text-black'>
+        <div className='col-6 col-md-3 p-md-0 text-black'>
           <span>{props.value}</span>
         </div>
       </div>
