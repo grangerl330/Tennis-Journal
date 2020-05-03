d2586c34bb2733de6dadf817679d5d4ad596c84d
grangerl330 <grangerl330@gmail.com>
2020-05-03 11:19:58 -0400

    remove unused import from ListPage

--- client/src/pages/ListPage.js
+++ client/src/pages/ListPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import $ from 'jquery'
-import matchesIcon from '../images/matches-icon.svg'
 import TitleRow from '../components/TitleRow'
 import ListContentRow from '../components/ListContentRow'
 
