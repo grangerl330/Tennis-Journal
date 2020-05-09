508b8af58f9ef22c5b6a94a03f4325b81155f2b8
grangerl330 <grangerl330@gmail.com>
2020-05-09 18:37:41 -0400

    update delete match modal

--- client/src/components/match-page/DeleteMatchModal.js
+++ client/src/components/match-page/DeleteMatchModal.js
@@ -9,20 +9,27 @@ const DeleteMatchModal = (props) => {
 
   return (
     <div className="modal fade" id="deleteMatchModal">
-      <div className="modal-dialog modal-md">
+      <div className="modal-dialog">
         <div className="modal-content">
-          <div className="modal-header bg-danger text-white">
-            <h5 className="modal-title">Delete Match</h5>
+          <div className="modal-header border-0">
             <button className="close" data-dismiss="modal">
-              <i className="fas fa-times"></i>
+              <i className="fas fa-times fa-sm text-grey"></i>
             </button>
           </div>
-          <div className="modal-body">
-            <p>Are you sure you want to delete this match?</p>
-          </div>
-          <div className="modal-footer justify-content-center">
-            <button className="btn btn-danger btn-block mr-4" data-dismiss="modal">No</button>
-            <button className="btn btn-success btn-block" data-dismiss="modal" onClick={deleteMatch}>Yes</button>
+          <div className="modal-body px-5">
+            <div className="row">
+              <div className="col">
+                <h5 className="text-green mb-4">Are you sure you want to delete this match?</h5>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-4">
+                <button className="btn mr-4 text-grey" data-dismiss="modal">Cancel</button>
+              </div>
+              <div className="col-4">
+                <button className="btn btn-green btn-block" data-dismiss="modal" onClick={deleteMatch}>Delete</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
